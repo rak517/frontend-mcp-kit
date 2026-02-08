eacf90f4b447303a0dc1621f9668d94363ecd0e5
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2026-02-08 16:21:37 +0900

    :sparkles: feat: 에러 메시지 파서 유틸 적용

--- packages/test-toolkit/src/runners/jest-runner.ts
+++ packages/test-toolkit/src/runners/jest-runner.ts
@@ -1,6 +1,7 @@
 import { spawn } from "node:child_process";
 import type { RunTestsOutput, TestResult } from "../tools/run-tests.js";
 import { NPX_BIN } from "../utils/constants.js";
+import { parseErrorMessage } from "../utils/parse-error-message.js";
 
 interface JestAssertionResult {
   title: string;
@@ -67,7 +68,7 @@ export async function runJest(
               duration: assertion.duration ?? 0,
               location: { file: testFile.name },
               error: assertion.failureMessages?.[0]
-                ? { message: assertion.failureMessages[0] }
+                ? parseErrorMessage(assertion.failureMessages[0])
                 : undefined,
             });
           }
